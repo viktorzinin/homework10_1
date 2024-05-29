9c396f0ce69467fe94e7663d01824b936651c88a
viktor <bbs-auto@list.ru>
2024-05-30 00:57:02 +0300

    update tests processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -7,3 +7,7 @@ def test_get_sort_dict(list_dict):
         {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
         {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
     ]
+
+
+def test_get_sort_dict_none():
+    assert get_date_sorted([]) == []
