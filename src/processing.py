c744580bc8be353d0dfc08f4b19e3110c5429e12
viktor <bbs-auto@list.ru>
2024-07-08 00:23:48 +0300

    init main.py created test_main.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,8 @@
+import re
+import collections
+from collections import Counter
+
+
 def get_sorted(list_dict: list, state: str = "EXECUTED") -> list:
     """Функуия фильтрует список словарей по ключу state"""
     filter_list = []
@@ -11,3 +16,29 @@ def get_date_sorted(list_dict: list, direction: bool = True) -> list:
     """Функция сортирует по дате"""
     sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=direction)
     return sorted_list
+
+
+def filter_dicts_by_string(dicts_to_filter, string):
+    """Функция принимает список словарей с данными о транзакциях и строку для поиска, и возвращает список словарей,
+    в которых присутствет строка для поиска"""
+
+    filtered_dicts_list = []
+    for dict in dicts_to_filter:
+        if "description" in dict:
+            match = re.findall(rf"{string}+", dict["description"], re.IGNORECASE)
+            if match:
+                filtered_dicts_list.append(dict)
+    return filtered_dicts_list
+
+
+def sort_dicts_by_categories(list_of_dicts, list_of_categories):
+    """Функция принимает список словарей с данными о банковских операциях и список категорий операций,
+    и возвращает словарь, в которой ключи - категории операций, а значения - количества транзакций
+    в каждой из перечислинных категорий"""
+
+    descriptions = [dict_["description"] for dict_ in list_of_dicts]
+    descriptions_counted = Counter(descriptions)
+    my_dict = collections.defaultdict(int)
+    for i in range(len(list_of_categories)):
+        my_dict[list_of_categories[i]] = descriptions_counted[list_of_categories[i]]
+    return my_dict
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import json
 import logging
-
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from src.external_api import currency_conversion
 
 logging.basicConfig(
     filename="logs/application.log",
@@ -8,24 +11,62 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s: %(message)s",
     level=logging.INFO,
 )
-
 logger = logging.getLogger("utils")
 
 
-def transaction_amount(file_path: str) -> list:
-    """Функцию, которая принимает на вход путь до JSON-файла
-    и возвращает список словарей с данными о финансовых транзакциях"""
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+headers = {"apikey": API_KEY}
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_transaction_json_data(path_to_json):
+    """Фунция принимает путь до файла и возвращает содержимое JSON файла"""
+
+    logger.info("Запущена функция get_transaction_info")
     try:
-        with open(file_path, "r", encoding="utf-8") as file:
-            repos = json.load(file)
-        if isinstance(repos, list):
-            logger.info("Возвращает список словарей")
-            return repos
-        else:
-            logger.info("Фаил пустой, содержит не список или не найден")
-            return []
-    except Exception as e:
-        logger.error(f"Произошла ошибка: {e}")
-        print(f"Ошибка {e}")
+        logger.info(f"Открытие JSON файла с указанным путём: {path_to_json}")
+        with open(path_to_json) as f:
+            try:
+                data = json.load(f)
+                logger.info("Получение данных из JSON файла")
+                return data
+            except json.JSONDecodeError as ex:
+                logger.error(f"Ошибка получения данных JSON формата: {ex}")
+                return []
+    except FileNotFoundError as ex:
+        logger.error(f"Ошибка, файл не найден: {ex}")
+        return []
+
+
+def get_transaction_csv_data(path_to_csv):
+    """Функция принимает путь до файла CSV и возвращает данные транзакций"""
+    with open(path_to_csv, "r", encoding="utf-8") as file:
+        data = pd.read_csv(file)
+        data_as_dict = data.to_dict(orient="records")
+        return data_as_dict
+
+
+def get_transaction_xlsx_data(path_to_xlsx):
+    """Функция принимает путь до файла XLSX и возвращает данные транзакций"""
+    with open(path_to_xlsx, "rb") as file:
+        data = pd.read_excel(file)
+        data_as_dict = data.to_dict(orient="records")
+        return data_as_dict
+
+
+def amount_of_transaction(transaction_json):
+    """Функция принимает JSON строку с информацией о транзакции и возвращает сумму данной транзакции"""
+    logger.info("Запуск функции amount_of_transaction")
+    try:
+        logger.info("Получение значения amount - суммы транзакции")
+        currency = transaction_json[0]["operationAmount"]["currency"]["code"]
+        amount = transaction_json[0]["operationAmount"]["amount"]
+        if currency != "RUB":
+            logger.info(f"Конвертация валюты {currency} в рубли")
+            amount = currency_conversion(currency, amount, API_KEY)
+        return amount
+    except KeyError as ex:
+        logger.info(f"Ошибка получения суммы транзакции: {ex}")
         return []
